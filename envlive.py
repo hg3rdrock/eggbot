3ff49207bdc5057322e8a21b6b5beb7a7941b65e
Hu Gang <gone.who@gmail.com>
2021-01-07 16:11:19 +0800

    create live trade env

---
+++ envlive.py
@@ -0,0 +1,105 @@
+import time
+
+import ccxt
+import numpy as np
+from gym import spaces, Env
+
+
+class HuobiLiveEnv(Env):
+
+    def __init__(self, simulate_mode=True, max_steps=100):
+        super(HuobiLiveEnv, self).__init__()
+        self.simulate_mode = simulate_mode
+        self.max_steps = max_steps
+
+        self.exc = ccxt.huobipro({
+            'apiKey': 'mk0lklo0de-bab860fb-e28d3608-eab36',
+            'secret': '3448139b-848dc9aa-9424d4e1-ccfa8',
+            'options': {
+                'createMarketBuyOrderRequiresPrice': False
+            }
+        })
+
+        self.pair1 = 'BTC/USDT'
+        self.pair2 = 'BTC3S/USDT'
+
+        self.n_lookback = 30
+        self.action_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=0, high=np.inf, shape=(2, self.n_lookback, 5), dtype=np.float)
+
+    def reset(self):
+        self.ts = 0
+        self.assets = np.array([0.1, 0.0], dtype=np.float)
+        self.n_realloc = 0
+
+        obs = self._make_obs()
+        self.init_balance = self._calc_balance()
+
+        return obs
+
+    def _make_obs(self):
+        h1 = self.exc.fetchOHLCV(self.pair1, '1h', limit=self.n_lookback)
+        h2 = self.exc.fetchOHLCV(self.pair2, '1h', limit=self.n_lookback)
+        self.price1 = h1[-1][-2]
+        self.price2 = h2[-1][-2]
+        h1, h2 = np.array(h1), np.array(h2)
+        return np.stack((h1[:, -5:], h2[:, -5:]))
+
+    def step(self, action):
+        begin_balance = self._calc_balance()
+
+        self._alloc_assets(action[0])
+
+        time.sleep(60 * 60)
+        self.ts += 1
+
+        done = self.ts >= self.max_steps
+        obs = self._make_obs()
+        end_balance = self._calc_balance()
+        reward = end_balance - begin_balance
+
+        if self.ts % 24 == 0:
+            print(f"=======step {self.ts}=======")
+            print(f"initial balance: {self.init_balance}")
+            print(f"end balance: {end_balance}")
+            print(f"end assets: {self.assets}")
+            print(f"total realloc: {self.n_realloc}")
+
+        return obs, reward, done, {}
+
+    def _alloc_assets(self, action):
+        total_usdt = np.dot(self.assets, np.array([self.price1, self.price2]))
+        frac1 = self.assets[0] * self.price1 / total_usdt
+
+        if action > frac1 + 0.01:
+            usdt_amt = (action - frac1) * total_usdt
+            if usdt_amt <= 5:
+                print("usdt amt too low")
+                return
+            self.assets[0] += usdt_amt * (1 - 0.002) / self.price1
+            self.assets[1] -= min(usdt_amt * (1 - 0.002) / self.price2, self.assets[1])
+            self.n_realloc += 1
+            print(f"timestep: {self.ts}")
+            print(f"sell btc3s and buy btc for {usdt_amt} USDT")
+            if not self.simulate_mode:
+                self.exc.create_market_sell_order(self.pair2, min(usdt_amt * (1 - 0.002) / self.price2, self.assets[1]))
+                self.exc.create_market_buy_order(self.pair1, usdt_amt)
+        elif action < frac1 - 0.01:
+            usdt_amt = (frac1 - action) * total_usdt
+            if usdt_amt <= 5:
+                print("usdt amt too low")
+                return
+            self.assets[0] -= usdt_amt * (1 - 0.002) / self.price1
+            self.assets[1] += usdt_amt * (1 - 0.002) / self.price2
+            self.n_realloc += 1
+            print(f"timestep: {self.ts}")
+            print(f"sell btc and buy btc3s for {usdt_amt} USDT")
+            if not self.simulate_mode:
+                self.exc.create_market_sell_order(self.pair1, min(usdt_amt * (1 - 0.002) / self.price1, self.assets[0]))
+                self.exc.create_market_buy_order(self.pair2, usdt_amt)
+
+    def _calc_balance(self):
+        return np.dot(self.assets, np.array([self.price1, self.price2]))
+
+    def saved_trades(self):
+        pass
---
+++ golive.py
@@ -0,0 +1,31 @@
+import logging
+
+from stable_baselines3 import A2C
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+from envlive import HuobiLiveEnv
+
+# Set up logging
+logging.basicConfig(
+    filename="/home/root/goldegg.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s : %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p"
+)
+
+# Load the model
+model = A2C.load('./trained_models/CryptoPfoA2C_20210106-17h03.zip')
+
+# Create live environment
+val_env = DummyVecEnv([lambda: HuobiLiveEnv()])
+obs = val_env.reset()
+done = False
+
+# Start runloop
+logging.info("Start trading...")
+
+while not done:
+    action, _ = model.predict(obs)
+    obs, _, done, _ = val_env.step(action)
+
+logging.info("End trading.")
